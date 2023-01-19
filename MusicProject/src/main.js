7f09c409f69e40430eab71fca4dfe7b435d68313
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-01-18 22:44:47 -0800

    Offline Support with service workers

--- MusicProject/src/main.js
+++ MusicProject/src/main.js
@@ -8,7 +8,8 @@ import "./assets/main.css";
 import { auth } from "./includes/firebase";
 import Icon from "./directives/icon";
 import i18n from "./includes/i18n";
-
+import { registerSW } from "virtual:pwa-register";
+registerSW({ immediate: true });
 let app;
 auth.onAuthStateChanged(() => {
   if (!app) {
